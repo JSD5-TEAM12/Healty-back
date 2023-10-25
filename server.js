ec4500a9deb5f28e1cbd946d8e8a42ece8d68ffb
unknown <honeytose@gmail.com>
2023-10-26 05:13:10 +0700

    increse size import data = 50mb

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ const port = process.env.PORT || 8050;
   //};
 
 // use middleware
-app.use(express.json({ limit: '10mb' }));
-app.use(bodyParser.json({limit : '10mb'}));
-app.use(express.urlencoded({extended:true}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({limit : '50mb'}));
 app.use(morgan('dev'));
 //app.use(cors(corsOptions));
 app.use(
