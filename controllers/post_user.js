315c3abd40f2a32eb767b54e54953d189b6bd3a4
unknown <honeytose@gmail.com>
2023-10-26 07:42:21 +0700

    delete debug3

--- controllers/post_user.js
+++ controllers/post_user.js
@@ -18,28 +18,10 @@ exports.read = async(req,res) =>{
         res.send('Server error').status(500)
     }
 }
-// exports.readall = async(req,res) =>{
-//     try{
-//         const postuser = await User_post.find({}).exec();
-//         res.send(postuser)
-//     }catch(err){
-//         console.log(err)
-//         res.send('Server error').status(500)
-//     }
-// }
-
 exports.create = async(req,res) =>{
     try {
 
         const {desc,image,user_post_id} = req.body
-        // const userPost = User_post()
-         
-        // console.log(req.file)
-        // let data = req.body
-        // if(req.file){
-        //     data.file = req.file.filename
-        // }
-        // const bufferToString = image2.buffer.toString('base64');
         const result = await cloudinary.uploader.upload(image, {
             folder: 'react-image',
             public_id: desc,
@@ -78,7 +60,6 @@ exports.deletePost = async (req, res) => {
       }
       await cloudinary.uploader.destroy(post.image.public_id);
       await User_post.findByIdAndRemove(postId);
-      console.log(postId)
   
       res.status(200).send('Post deleted successfully' );
     } catch (err) {
@@ -86,22 +67,3 @@ exports.deletePost = async (req, res) => {
       res.status(500).send('Server error');
     }
   }
-
-// exports.deletepost = async(req,res) =>{
-//     try{
-//         id = req.params.id
-//         const removeed = await User_post.findOneAndDelete({_id:id}).exec()
-//         await fs.unlink(`./uploads/${removeed.file}`,(err)=>{
-//             if(err){
-//                 console.log(err)
-//             }
-//             else{
-//                 console.log('Remove success')
-//             }
-//         })
-//     }
-//     catch(err){
-//         console.log(err)
-//         res.send('Sever err').status(500)
-//     }
-// }
